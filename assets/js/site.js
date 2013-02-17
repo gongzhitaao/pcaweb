26d77341bf5c26a1c99600918f5f50da575bb7f0
Zhitao Gong <zhitaao.gong@gmail.com>
2013-02-16 21:06:43 -0600

    url thing

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,19 +2,22 @@
 (function ($) {
 
     var menuidx = {
-        '': 1,
-        'comp1000': 2,
-        'comp1003': 3,
-        'comptest': 4,
-        'itminor': 5,
-        'k12': 6
+        '/pcaweb/': 1,
+        '/pcaweb/comp1000/': 2,
+        '/pcaweb/comp1003/': 3,
+        '/pcaweb/comptest/': 4,
+        '/pcaweb/itminor/': 5,
+        '/pcaweb/k12/': 6
     };
 
     $(document).ready(function() {
 
         // highlight main menu
-        var path = window.location.pathname.split('/')[1];
+        var path = window.location.pathname;
         $('#mainnav li:nth-child(' + menuidx[path] + ')').addClass('active');
+
+
+        alert('helo');
 
         $('#pagetab a:first').tab('show');
         $('#slider').nivoSlider();
