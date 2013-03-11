860746e481a8e8f97b1cc6009ff35776cfd6a24e
Zhitao Gong <gongzhitaao@gong-kub.(none)>
2013-03-10 23:08:03 -0500

    1. change structure 2. change left nav to accordion 3. change default font to 'Open Sans' from google font 4. add k12 content frame 5. cleaning up

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,20 +5,41 @@
 (function ($) {
 
     var menuidx = {
-        '/pcaweb/': 1,
-        '/pcaweb/courses/': 2,
-        '/pcaweb/k12/': 3,
-        '/pcaweb/help/': 4,
+        '/': 1,
+        '/courses': 2,
+        '/k12': 3,
+        '/help': 4,
+    };
+
+    var o = {
+        titleClass: 'ac-title',
+        controlClass: 'ac-ctrl',
+        expandHtml: '&#9662;',
+        collapsedHtml: '&#9656;'
     };
 
     $(document).ready(function() {
 
         // highlight main menu
         var path = window.location.pathname;
-        $('#mainnav li:nth-child(' + menuidx[path] + ')').addClass('active');
+        var tmp = path.split('/');
+        $('#mainnav li:nth-child(' + menuidx['/' + tmp[1]] + ')').addClass('active');
 
-        $('#pagetab a:first').tab('show');
-        $('#slider').nivoSlider();
+        var f = $("<span></span>", {'class': o.controlClass}).html(o.collapsedHtml)
+        $('.' + o.titleClass).before(f);
+
+        if (tmp[2]) $('#' + tmp[2]).addClass('in');
+
+        $('.collapse')
+            .on('hide', function() {
+
+            })
+            .on('show', function() {
+
+            });
+        ;
+
+
     });
 
 }(jQuery));
